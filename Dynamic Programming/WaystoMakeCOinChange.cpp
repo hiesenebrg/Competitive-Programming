dfa331263099911d94b5c563018e07a53ff648a1
Adarsh Singh <93775283+hiesenebrg@users.noreply.github.com>
2024-08-30 10:16:15 +0530

    Create WaystoMakeCOinChange.cpp
    Problem statement
    You are given an infinite supply of coins of each of denominations D = {D0, D1, D2, D3, ...... Dn-1}. You need to figure out the total number of ways W, in which you can make a change for value V using coins of denominations from D. Print 0, if a change isn't possible.
    
    Detailed explanation ( Input/output format, Notes, Images )
    Sample Input 1 :
    3
    1 2 3
    4
    Sample Output 1:
    4
    Explanation for Sample Output 1:
    We can make a change for the value V = 4 in four ways.
    1. (1,1,1,1), 
    2. (1,1, 2), [One thing to note here is, (1, 1, 2) is same as that of (2, 1, 1) and (1, 2, 1)]
    3. (1, 3), and 
    4. (2, 2)
    Sample Input 2 :
    3
    5 3 2
    1
    Sample Output 2:
    0

---
+++ Dynamic Programming/WaystoMakeCOinChange.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+long minimumElementsHelper(int *denominations, int x, int n,vector<vector<long>> &dp)
+{
+    if(n==0){
+      return (x%denominations[0]==0);
+        
+    }
+    if(dp[n][x]!=-1) return dp[n][x];
+    long pick = 0;
+    if(denominations[n]<=x) pick =  minimumElementsHelper(denominations, x-denominations[n],n,dp);
+    long notPick = 0+ minimumElementsHelper(denominations, x,n-1,dp);
+    return dp[n][x] = pick+notPick;
+}
+
+    
+
+
+long countWaysToMakeChange(int *denominations, int n, int value)
+{
+   
+      vector<vector<long>>dp(n,vector<long>(value+1,-1));
+      return   minimumElementsHelper(denominations, value, n-1,dp);
+       
+}
