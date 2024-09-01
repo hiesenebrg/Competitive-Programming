d6b25bf8685733b920c41d059b46f34dc926cd31
hiesenebrg <adarshsingh8008@gmail.com>
2024-09-01 11:20:07 +0530

    LCS and print LCS added

---
+++ Dynamic Programming/PrintLCS.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int lcsHelper(string s, string t, int i, int j,vector<vector<int>> &dp)
+{
+	if(i==0 || j==0) return  0;
+	if(dp[i][j]!=-1) return dp[i][j];
+	if(s[i-1] == t[j-1]) return  dp[i][j] =   1 + lcsHelper(s,t,i-1,j-1,dp);
+	return dp[i][j] =  max(lcsHelper(s,t,i-1,j,dp),lcsHelper(s,t,i,j-1,dp)) ;
+
+
+}
+int lcs(string s, string t) {
+    int m = s.size();
+    int n = t.size();
+// vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
+// 	// return  lcsHelper(s,t,s.size(), t.size(),dp);
+    vector<vector<int>> dp(m+1, vector<int>(n+1, 0));
+    for (int i = 1; i <= m; i++) {
+      for (int j = 1; j <= n; j++) {
+        if (s[i - 1] == t[j - 1])
+          dp[i][j] = 1 + dp[i - 1][j - 1];
+        else
+          dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+      }
+    }
+
+    
+    // return dp[m][n];
+  int len =  dp[m][n];
+  string ans = "";
+  for( int k =0;k<len;k++)  ans+='$';
+  int i = m;
+  int j =n;
+   while(i>0 && j>0){
+            if (s[i-1] == t[j-1]){
+              ans[len-1] = s[i-1];
+              len-=1;
+               i-=1;
+               j-=1;
+            }
+               
+            else if (dp[i][j-1]> dp[i-1][j]){
+              j-=1;
+            }else{
+                i-=1;
+            }
+        
+    }
+    cout << ans <<endl;
+    return 0;
+}
+
--- Recursion/Return/subSequnce.cpp
+++ Recursion/Return/subSequnce.cpp
@@ -1,23 +1,38 @@
-
 #include <iostream>
-using namespace std;
-#include <string>
 #include <vector>
-// since this is tree view , remember base case hits many times
-vector<string> getSubsequences(string str, string curr = "", int index = 0) {
-    vector<string> result;
+using namespace std;
+
+// Function to generate all subsequences of a given string
+void generateSubsequences(string str, string output, int index, vector<string> &subsequences) {
+    // Base case: if the current index reaches the end of the string
     if (index == str.length()) {
-        result.push_back(curr);
-        return result;
+        subsequences.push_back(output);  // Store the current subsequence
+        return;
     }
 
-    // Exclude the current character
-    vector<string> exclude = getSubsequences(str, curr, index + 1);
-    result.insert(result.end(), exclude.begin(), exclude.end());
+    // Exclude the current character and move to the next character
+    generateSubsequences(str, output, index + 1, subsequences);
 
-    // Include the current character
-    vector<string> include = getSubsequences(str, curr + str[index], index + 1);
-    result.insert(result.end(), include.begin(), include.end());
+    // Include the current character in the subsequence and move to the next character
+    output += str[index];
+    generateSubsequences(str, output, index + 1, subsequences);
+}
 
-    return result;
+// Function to return all subsequences of a given string
+vector<string> getAllSubsequences(string str) {
+    vector<string> subsequences;
+    generateSubsequences(str, "", 0, subsequences);
+    return subsequences;
+}
+
+int main() {
+    string str = "abc";  // Example string
+    vector<string> subsequences = getAllSubsequences(str);
+
+    // Print all subsequences
+    for (const string &subseq : subsequences) {
+        cout << "'" << subseq << "'" << endl;
+    }
+
+    return 0;
 }
