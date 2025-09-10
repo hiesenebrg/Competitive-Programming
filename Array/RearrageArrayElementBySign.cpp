296a6f5d1b946c6bc3f18f9b5830d6fd721045f9
hiesenebrg <adarshsingh8008@gmail.com>
2025-09-10 08:47:14 +0530

    rearrange Array

---
+++ Array/RearrageArrayElementBySign.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& nums) {
+        int even=0;
+        int odd =1;
+        vector<int> ans(nums.size());
+             for(int i=0;i<nums.size();i++){
+            if(nums[i]<0){
+                 ans[odd] = nums[i];
+                 odd+=2;
+            }
+            else{
+                ans[even] = nums[i];
+                 even+=2;
+            }
+        }
+        return ans;
+    }
+};
